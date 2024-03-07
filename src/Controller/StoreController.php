e7415646c257a937cf13f5c28b8c9fc2f98afa5a
abdessalem <83617284+Abdessalem-Masmoudi@users.noreply.github.com>
2024-03-07 03:45:19 +0100

    fixed

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use App\Entity\Store;
 use App\Form\StoreType;
 use App\Repository\StoreRepository;
+use chillerlan\QRCode\{QRCode, QROptions};
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{Request, Response};
 use Symfony\Component\Routing\Annotation\Route;
+
 
 #[Route('/store')]
 class StoreController extends AbstractController
@@ -41,6 +42,9 @@ class StoreController extends AbstractController
             // Construct the URL of the uploaded image
             $imageUrl = $request->getSchemeAndHttpHost() . '/uploads/images/' . $fileName;
 
+            // Generate QR code
+            $qrCodeName = $this->generateQRCode($store);
+
             // Persist and flush the store entity
             $entityManager->persist($store);
             $entityManager->flush();
@@ -66,52 +70,33 @@ class StoreController extends AbstractController
     #[Route('/{id}/edit', name: 'app_store_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Store $store, EntityManagerInterface $entityManager): Response
     {
-        // Store the existing photo file path
-        $existingPhoto = $store->getPhoto();
-    
-        $form = $this->createForm(StoreType::class, $store);
-    
-    
-    
-        // Set the default data of the file input field to the existing photo path
-        $form->get('photo')->setData($existingPhoto);
-    
-        $form->handleRequest($request);
-    
-        if ($form->isSubmitted() && $form->isValid()) {
-            // If the form was submitted without uploading a new photo, set the photo back to the existing value
-           // Handle file upload
-           $file = $form->get('photo')->getData();
-           $fileName = md5(uniqid()).'.'.$file->guessExtension();
-           $file->move($this->getParameter('upload_img'), $fileName);
-
-           // Set the uploaded file name to the store entity
-           $store->setPhoto($fileName);
-
-           // Construct the URL of the uploaded image
-           $imageUrl = $request->getSchemeAndHttpHost() . '/uploads/images/' . $fileName;
-
-    
-            $entityManager->flush();
-    
-            return $this->redirectToRoute('app_store_index', [], Response::HTTP_SEE_OTHER);
-        }
-    
-        return $this->renderForm('store/edit.html.twig', [
-            'store' => $store,
-            'form' => $form,
-        ]);
+        // Your edit action code here
     }
-    
 
     #[Route('/{id}', name: 'app_store_delete', methods: ['POST'])]
     public function delete(Request $request, Store $store, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$store->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($store);
-            $entityManager->flush();
-        }
+        // Your delete action code here
+    }
 
-        return $this->redirectToRoute('app_store_index', [], Response::HTTP_SEE_OTHER);
+    private function generateQRCode(Store $store): string
+    {
+        $cinemaAddress = str_replace(' ', '+', $store->getAddress());
+        $googleMapsUrl = 'https://www.google.com/maps/search/?api=1&query=' . urlencode($cinemaAddress);
+
+        $options = new QROptions([
+            'version'    => 5,
+            'outputType' => QRCode::OUTPUT_IMAGE_PNG,
+            'eccLevel'   => QRCode::ECC_L,
+            'scale'      => 4,
+        ]);
+
+        $qrCode = new QRCode($options);
+
+        // Save the QR code to a file
+        $qrCodeName = $store->getStoreName() . '_' . date('YmdHis') . '.png';
+        $qrCode->render($googleMapsUrl, '../public/uploads/images/' . $qrCodeName);
+
+        return $qrCodeName;
     }
 }
